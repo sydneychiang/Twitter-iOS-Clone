135e493c1d37eb8479d54f88396c954a1c371550
Sydney Chiang <sjchiang@uci.edu>
2021-01-26 01:45:18 -0500

    connected api to table cells, added infinite scroll

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Sydney Chiang on 1/25/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
